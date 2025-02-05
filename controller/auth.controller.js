104c05876679051140cd6deb818d80f890d51806
ohey <huynhgiabao853@gmail.com>
2025-02-05 10:52:52 +0700

    4

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -33,34 +33,40 @@ export const signup = async (req, res, next) => {
 }
 
 export const signin = async (req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   try {
-    const validUser = await User.findOne({ email })
+    const validUser = await User.findOne({ email });
 
     if (!validUser) {
-      return next(errorHandler(404, "User not found"))
+      return next(errorHandler(404, "User not found"));
     }
 
-    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
 
     if (!validPassword) {
-      return next(errorHandler(401, "Wrong Credentials"))
+      return next(errorHandler(401, "Wrong Credentials"));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const token = jwt.sign(
+      { id: validUser._id }, 
+      process.env.JWT_SECRET, 
+      { expiresIn: "7d" }
+    );
 
-    const { password: pass, ...rest } = validUser._doc
+    const { password: pass, ...rest } = validUser._doc;
 
-    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+    res.status(200).json({
       success: true,
       message: "Đăng nhập thành công!",
-      rest,
-    })
+      token,
+      user: rest,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
 
 export const signout = async (req, res, next) => {
   try {
